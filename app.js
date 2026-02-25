46f8c1fac633c1c88526c11e91fd540183480720
newcodesubed <subedshahxi@gmail.com>
2026-02-25 17:19:17 +0545

    feat(router): add route for url shorten

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+import bodyParser from 'body-parser';
+import express from "express"
 
-const express = require('express');
+import routers from './src/routes/index.routes';
 PORT= 3001
 const app = express();
+
+// Parse JSON bodies (application/json)
+app.use(bodyParser.json());
+
+app.use('/urlShorten/v1', routers);
+
 
 app.listen(PORT,()=>{
     console.log(`App running on port ${PORT}`);
